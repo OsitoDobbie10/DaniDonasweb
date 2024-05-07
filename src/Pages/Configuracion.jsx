4d81be587285bd8ffb4d34d019547fb08348db3e
ositodobbie10 <caamledzeppelin@gmail.com>
2024-05-06 18:12:49 -0600

    "Primer cambio 20240506"

--- src/Pages/Configuracion.jsx
+++ src/Pages/Configuracion.jsx
@@ -1,6 +1,7 @@
-import styled from "styled-components"
-import {v,HeaderDonna} from "../index"
-import { useNavigate } from "react-router-dom"
+import styled from "styled-components";
+import {v,HeaderDonna} from "../index";
+import { useNavigate } from "react-router-dom";
+import Celular from "../Assets/AddNumber.png";
 const Configuracion = ({datosgenerales}) => {
   return (
     <Container>
@@ -9,6 +10,15 @@ const Configuracion = ({datosgenerales}) => {
     <div className="DatosGeneralesCuenta">
     <span className="TuNombre">{datosgenerales.user.user_metadata.first_name}</span>
     <span className="TuEmail">{datosgenerales.user.user_metadata.email}</span>
+    </div>
+    <div className="AddMyPhone">
+    <img src={Celular} alt="AddPhone" className="ImagenAddMyPhone"/>
+    <span className="SimboloCel">+</span>
+    </div>
+    <div className="encabezadoUsuario">
+    <span className="IconoUser">{<v.User/>}</span>
+    <span className="ConfigPerfil">Configuracion del perfil</span>
+    <span className="EdicionUsuario">Editar</span>
     </div>
     </div>
     </Container>
@@ -21,6 +31,7 @@ const Container = styled.div`
   margin: 10px auto;
   display: flex;
   flex-direction: column;
+  position: relative;
   .DatosGeneralesCuenta{
     display: flex;
     flex-direction: column;
@@ -40,6 +51,51 @@ const Container = styled.div`
       text-align: center;
     }
   }
+
+  .AddMyPhone{
+    position: absolute;
+    top:20px;
+    right:10px;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    gap:5px;
+    .ImagenAddMyPhone{
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+    }
+    .SimboloCel{
+      font-size: 25px;
+      font-weight: bold;
+    }
+
+  }
+  .encabezadoUsuario{
+      display: flex;
+      justify-content: space-between;
+      margin: 20px auto;
+      width: 90%;
+    .IconoUser{
+      font-size: 30px;
+      font-weight: bold;
+      color: gray;
+    }
+    .ConfigPerfil{
+      font-size: 30px;
+      font-weight: bold;
+      color: gray;
+    }
+    .EdicionUsuario{
+      font-size: 30px;
+      font-weight: bold;
+      color: gray;
+      &:hover{
+        color: black;
+        cursor: pointer;
+      }
+    }
+    }
 }
 `;
 export default Configuracion

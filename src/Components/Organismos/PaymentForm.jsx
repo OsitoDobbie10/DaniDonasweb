2b04d77ca6e438bdb444f9629c15245554413255
ositodobbie10 <caamledzeppelin@gmail.com>
2024-05-12 13:16:48 -0600

    "Instalar dependencias node 18 "

---
+++ src/Components/Organismos/PaymentForm.jsx
@@ -0,0 +1,20 @@
+import {v} from "../../index"
+import styled from "styled-components"
+const PaymentForm = () => {
+  return (
+    <Container>
+    <div className="card">
+
+    </div>
+    </Container>
+  )
+}
+const Container = styled.div`
+display: inline-block;
+border-radius: 10px;
+box-shadow: 0 0 10px #dadada;
+.card{
+    
+}
+`;
+export default PaymentForm
--- src/Pages/Targetas.jsx
+++ src/Pages/Targetas.jsx
@@ -4,14 +4,24 @@ const Targetas = () => {
   return (
     <Container>
     <HeaderDonna icono={<v.Atras/>} texto="Formulario de Targeta" icono2={<v.HOME/>} ruta="/Configuracion"/>
+    <div className="layout">
+    </div>
+
     </Container>
   )
 }
 const Container = styled.div`
 width: 100%;
 height: 100%;
+background-color: #fafafa;
 display: flex;
 flex-direction: column;
+.layout{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
 
 `;
 export default Targetas;
